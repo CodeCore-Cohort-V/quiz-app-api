d92da65b9302c2b9e92a77b9dcfdbd33d7afc9ab
Mike Martinson <mike.j.martinson@gmail.com>
2014-11-15 12:38:17 -0800

    Implement sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
   def create
+    @user = User.find_by_email params[:email]
+    if @user && @user.authenticate params[:password]
+      session[:user_id] @user.id
+      redirect_to root_path, notice: "We did it Martha, we're in!"
+    else
+      flash.now[:alert] = "You messed something up. Either type it right or created a new account"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/welcome'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 
   validates: :email, uniqueness: true, presence: true,
    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: [:create, :update] }
+
 end
