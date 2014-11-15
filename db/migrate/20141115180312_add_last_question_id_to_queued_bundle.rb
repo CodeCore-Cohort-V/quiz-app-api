5827536ff618886283c0d46cff193fd0c98cf497
Mike Martinson <mike.j.martinson@gmail.com>
2014-11-15 13:53:05 -0800

    Fix migrations

--- db/migrate/20141115180312_add_last_question_id_to_queued_bundle.rb
+++
@@ -1,5 +0,0 @@
-class AddLastQuestionIdToQueuedBundle < ActiveRecord::Migration
-  def change
-    add_column :queuers, :last_question_id, :integer, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141115204029) do
   end
 
   create_table "queuers", force: true do |t|
+    t.integer  "last_question_id"
     t.integer  "user_id"
     t.integer  "bundle_id"
     t.datetime "created_at"
