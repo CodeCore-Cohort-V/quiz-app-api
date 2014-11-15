95712d038ab957ffd6c1091caf7a49bdf7ea6d37
daniel-johnson <janiel.dohnson@gmail.com>
2014-11-15 12:36:26 -0800

    added content column to Choices table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115201359) do
+ActiveRecord::Schema.define(version: 20141115203107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141115201359) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "correct"
+    t.string   "content"
   end
 
   create_table "questions", force: true do |t|
