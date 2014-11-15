76d43b3f8b6782ceb199e8a23531a61574615f94
daniel-johnson <janiel.dohnson@gmail.com>
2014-11-15 12:44:01 -0800

    changed content column in Questions table from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115203829) do
+ActiveRecord::Schema.define(version: 20141115204029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,13 +45,13 @@ ActiveRecord::Schema.define(version: 20141115203829) do
   end
 
   create_table "questions", force: true do |t|
-    t.string   "content"
     t.string   "type"
     t.integer  "bundle_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "position"
     t.text     "example"
+    t.text     "content"
   end
 
   create_table "queuers", force: true do |t|
