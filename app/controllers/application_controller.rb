8f234ac47421eaaeb45dba07c4398ced20a87482
Mike Martinson <mike.j.martinson@gmail.com>
2014-11-15 14:29:58 -0800

    fix formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
 
   def user_signed_in?
     session[:user_id].present?
@@ -17,8 +21,5 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  def sign_in(user)
-    session[:user_id] = user.id
-  end
 
 end
