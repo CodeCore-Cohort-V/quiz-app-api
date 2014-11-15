6802542d92a61fa29a358a810c341919473dbad1
Mike Martinson <mike.j.martinson@gmail.com>
2014-11-15 12:22:04 -0800

    Implement users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates: :email, uniqueness: true, presence: true,
+   format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: [:create, :update] }
 end
