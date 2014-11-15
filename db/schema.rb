738ec6058bfc0676889cded72677d209e929239a
daniel-johnson <janiel.dohnson@gmail.com>
2014-11-15 12:39:39 -0800

    added example column to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115203107) do
+ActiveRecord::Schema.define(version: 20141115203829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141115203107) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "position"
+    t.text     "example"
   end
 
   create_table "queuers", force: true do |t|
